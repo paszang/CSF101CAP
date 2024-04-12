24fb5fc5cd2f9e9367cb86ad35d3858495962926
unknown <02230268.cst@rub.edu.bt>
2024-04-12 22:42:05 +0530

    changes to CSF101CAP

--- CAP_02230268.py
+++ CAP_02230268.py
@@ -22,7 +22,7 @@ def read_input():
 # solution function to calculate the score
 def calculate_score(text):
     score=0
-    storedict= {'A X': 2, 'A Y': 4, 'A Z': 9, 'B X': 1, 'B Y': 5, 'B Z': 7, 'C X': 1, 'C Y': 6, 'C Z': 7} 
+    storedict= {'A X':3, 'A Y': 4, 'A Z': 8, 'B X': 1, 'B Y': 5, 'B Z': 9, 'C X': 2, 'C Y': 6, 'C Z': 7} 
     #dict containing score for each combination
     for file in text:
         newvalue=file.strip()   
